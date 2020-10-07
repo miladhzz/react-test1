b47108924ea823459d1128a5aab27c2c744f4f7f
M.Rozan <maryam.rozanpoor@gmail.com>
2020-10-07 11:30:40 +0330

    change bootstrapToReactBootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import {Badge,Button,Alert} from 'react-bootstrap';
 import Items from './Components/Item/Items';
 
 //import 'App.css';
@@ -51,14 +52,14 @@ class App extends React.Component{
   render(){
     const {items,showitem}=this.state;
     let item=null;  
-    let badgestyle=[];
+    let badgestyle='';
 
     if (items.length>=3)
-      badgestyle.push("badge-success"); 
+      badgestyle="success";
     if (items.length<=2)
-      badgestyle.push("badge-warning"); 
+      badgestyle="warning";
     if (items.length<=1)
-      badgestyle.push("badge-danger"); 
+      badgestyle="danger";
 
     if(showitem){
         item=<Items items={items} DeleteItem={this.handleDeleteItem} changeName={this.handleChangeName}/>
@@ -66,25 +67,26 @@ class App extends React.Component{
     
     return(
       <div className="rtl text-center">
-        <div className="alert alert-info">
+        <Alert variant="info">
           <h2>مدیریت کننده اقلام</h2>
-        </div>
+        </Alert>    
         <div>
-          <h5 className="alert alert-light">  تعداد اقلام  &nbsp;
-            <span className={`badge badge-pill ${badgestyle.join(" ")}`}>{items.length} 
-            </span> عدد می باشد 
-          </h5>
+          <Alert variant="light">
+          تعداد اقلام  &nbsp;
+            <Badge pill variant={badgestyle}>{items.length} 
+            </Badge> عدد می باشد 
+          </Alert>        
         </div>  
         <div className="m-s p-2">
           <form className="form-inline justify-content-center" onSubmit={event=>event.preventDefault()}>
             <div  className="input-group">
               <input type="text" className="form-control" placeholder=" اسم بهم بده " onChange={this.setItem} value={this.state.item}/>
-              <button type="submit" onClick={this.handleNewItem} className="btn btn-success fa fa-plus-square input-group-prepend"/>
+              <Button type="submit" onClick={this.handleNewItem} variant={"success"}className="fa fa-plus-square input-group-prepend"/>
             </div>
           </form>
         </div>
         <div>
-          <button onClick={this.handleShowItem} className={showitem ? "btn btn-info":"btn btn-danger"}>نمایش و مخفی</button>
+          <Button onClick={this.handleShowItem} variant={showitem ? "info":"danger"}>نمایش و مخفی</Button>
         </div>
 
         {item}
