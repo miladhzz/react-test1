edf788c468dc35861cbc982751cb8c9530a9bfea
m.hatami <m.hatami@son.ir>
2020-10-12 09:43:56 +0330

    add backend code

---
+++ ReactFactorApi/SelfHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:3001"))
+            {
+                Console.WriteLine("webserver running");
+                Console.WriteLine("pree any key to quit");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
