933b2249f87308cab2fe7aa7983361aff6d22494
M.Rozan <maryam.rozanpoor@gmail.com>
2020-10-20 14:58:08 +0330

    navbar

--- ReactFactorApi/SelfHost/client/src/index.js
+++ ReactFactorApi/SelfHost/client/src/index.js
@@ -4,20 +4,20 @@ import App from "./App";
 import Search from "./Components/Item/search"
 import * as serviceWorker from "./serviceWorker";
 import "react-toastify/dist/ReactToastify.css";
-import { Route, BrowserRouter as Router, Link  } from 'react-router-dom'
+import { Route, BrowserRouter as Router, NavLink  } from 'react-router-dom'
 
 const routing = (
   <Router>
-    <div>
-    <ul>
-        <li>
-          <Link to="/">خانه</Link>
-        </li>
-        <li>
-          <Link to="/search">جستجو</Link>
-        </li>
-        
+    <div className="m-3">
+    <ul className="nav nav-pills justify-content-center">
+      <li>
+          <NavLink to="/search" className="nav-link">جستجو</NavLink>
+      </li>
+      <li className="nav-item">
+          <NavLink to="/" className="nav-link" exact>خانه</NavLink>
+       </li>
       </ul>
+      <br/>
       <Route exact  path="/" component={App} />
       <Route path="/search" component={Search} />
     </div>
